1d6d64933db13612594ab84e5f6de99f305c8364
KAIF ANSARI <64328313+lovepains@users.noreply.github.com>
2020-04-27 11:52:25 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,311 @@
+
+#!/bin/bash
+echo $red
+echo ""----------------------------------------------------------------------------- ""
+echo " This Script is written by K2 LOVER start"
+echo "---------------------------------------------------------------------------------"
+
+echo "007"
+echo "007"
+echo "007"
+echo "."                                                        
+echo  "#####################################"
+echo   " we are lovers"                     echo   " we are lovers ok "                                                                                               
+echo   "####################################"
+echo "007"
+echo "007"                                  echo "007"
+echo "."
+apt-get update -y                      
+#update in progress
+apt install python -y       
+#installation......
+apt install python2 -y
+apt install ruby -y
+apt install git -y
+apt install screenfetch -y
+apt install cowsay -y
+apt install toilet -y
+apt install figlet -y
+echo  "#####################################"
+echo   " u can now do anything      "
+echo   " completing wait 3 2 1 "                               
+echo   "####################################"
+
+apt install php -y
+apt install perl -y
+apt install nmap -y
+pkg install openssh -y
+echo  "#####################################"
+
+echo   "join us for pain in love"                                                       
+echo   "####################################"
+
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+
+echo " "                                                        
+echo   "####################################"
+
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+apt install fish -y
+apt install unzip -y
+apt install tor -y
+apt install sudo -y
+apt install wget -y
+apt install tor -y
+apt install w3m -y
+apt install cmatrix -y
+echo  "#####################################"
+echo   "we are lovers "
+
+echo   "####################################"
+
+apt install wcalc -y
+apt install openssl
+apt install bmon -y
+
+echo  "#####################################"
+
+echo   " processing wait......
+"                                                               
+echo   "####################################"
+
+pkg update && pkg upgarde
+
+clear
+echo "STATUS 40 PERCENT COMPLETED" 
+
+echo   "NOW TURN FOR GIT CLONE PACKAGES "
+
+pkg update && pkg upgarde
+
+cd $HOME
+git clone  https://github.com/noob-hackers/T-LOAD
+git clone https://github.com/noob-hackers/tunnel
+git clone https://github.com/noob-hackers/grabcam
+git clone https://github.com/noob-hackers/Infect
+git clone https://github.com/Ublimjo/Termux-login
+git clone https://github.com/thelinuxchoice/shellphish
+git clone https://github.com/sundowndev/PhoneInfoga
+git clone https://github.com/thewhiteh4t/seeker
+git clone https://github.com/lovepains/Shortit
+
+clear
+
+echo "STATUS 50 PERCENT COMPLETED"
+
+echo    "NOW THEIR PERMISSIONS "
+
+pkg update && pkg upgarde
+
+cd $HOME
+cd T-LOAD
+chmod +x *
+cd $HOME
+cd tunnel
+chmod +x *
+cd $HOME
+cd grabcam
+chmod +x *
+cd $HOME
+cd Infect
+chmod +x *
+cd $HOME
+cd Termux-login
+chmod +x *
+cd $HOME
+cd shellphish
+chmod +x *
+cd $HOME
+cd PhoneInfoga
+chmod +x *
+cd $HOME
+cd seeker
+chmod +x *
+cd $HOME
+clear
+
+echo " "ALL FILES ARE CLONED AND GIVEN PERMISSION | lolcat 
+
+
+echo " ??? now rock and spread love ??? "
+
+Clear
+
+cd $HOME
+cd SingleSetup
+cd Shortcut-Handles
+clear
+mv @gp /$HOME/SingleSetup
+clear
+mv @installng /$HOME/SingleSetup
+clear
+mv @seeker /$HOME/SingleSetup
+clear
+mv @tunnel /$HOME/SingleSetup
+clear
+mv @grabcam /$HOME/SingleSetup
+clear
+mv @ngrok /$HOME/SingleSetup
+clear
+mv @shellphish /$HOME/SingleSetup
+clear
+mv @up /$HOME/SingleSetup
+clear
+mv @infect /$HOME/SingleSetup
+clear
+mv @pswd /$HOME/SingleSetup
+clear
+mv loveme /$HOME/SingleSetup
+clear
+mv @tload /$HOME/SingleSetup
+clear
+mv motd /$HOME/SingleSetup
+clear
+mv bash.bashrc /$HOME/SingleSetup
+clear
+mv c /$HOME/SingleSetup
+clear
+
+
+
+
+echo "STATUS 70 PERCENT COMPLETED"
+
+pkg update && pkg upgarde
+
+cd $HOME
+cd SingleSetup
+cp loveme /data/data/com.termux/files/usr/bin
+
+cd $HOME
+cd SingleSetup
+cp c /data/data/com.termux/files/usr/bin
+
+
+cd $HOME
+cd SingleSetup
+cp @installng /data/data/com.termux/files/usr/bin
+
+cd $HOME
+cd SingleSetup
+cp @tload /data/data/com.termux/files/usr/bin
+
+
+cd $HOME
+cd SingleSetup
+cp @pswd /data/data/com.termux/files/usr/bin
+
+
+
+cd $HOME
+cd SingleSetup
+cp @grabcam /data/data/com.termux/files/usr/bin
+
+cd $HOME
+cd SingleSetup
+cp @up /data/data/com.termux/files/usr/bin
+
+cd $HOME
+cd SingleSetup
+cp @ngrok /data/data/com.termux/files/usr/bin
+
+
+cd $HOME
+cd SingleSetup
+cp @infect /data/data/com.termux/files/usr/bin
+
+
+cd $HOME
+cd SingleSetup
+cp @seeker /data/data/com.termux/files/usr/bin
+
+cd $HOME
+cd SingleSetup
+cp @tunnel /data/data/com.termux/files/usr/bin
+
+
+cd $HOME
+cd SingleSetup
+cp @shellphish /data/data/com.termux/files/usr/bin
+
+
+cd $HOME
+cd SingleSetup
+cp @ /data/data/com.termux/files/usr/bin
+
+cd /data/data/com.termux/files/usr/bin
+chmod +x *
+
+cd $HOME
+cd T-LOAD
+rm -rf bash.bashrc
+
+cd  $HOME
+cd SingleSetup
+cp bash.bashrc /$HOME/T-LOAD
+
+cd $HOME
+cd Shortit 
+chmod +x *
+bash setup.sh
+
+apt-get update -y
+echo " "
+apt-get upgrade -y
+echo " "
+pkg install figlet -y
+echo " "
+pkg install toilet -y
+echo " "
+pkg install cowsay -y
+echo " "
+pkg install nano -y
+echo " "
+pkg install ruby -y
+echo " "
+gem install lolcat
+echo " "
+pkg install nano -y
+echo ""
+pkg install mpv -y
+echo " "
+
+clear
+pkg install toilet -y
+pkg install lolcat -y
+pkg update && pkg upgarde
+clear
+
+cd $HOME 
+cd SingleSetup
+mkdir Shortcut-Handles 
+mv @gp /$HOME/SingleSetup/Shortcut-Handles
+mv @installng /$HOME/SingleSetup/Shortcut-Handles
+mv @seeker /$HOME/SingleSetup/Shortcut-Handles
+mv @tunnel /$HOME/SingleSetup/Shortcut-Handles
+mv @grabcam /$HOME/SingleSetup/Shortcut-Handles
+mv @ngrok /$HOME/SingleSetup/Shortcut-Handles
+mv @shellphish /$HOME/SingleSetup/Shortcut-Handles
+mv @up /$HOME/SingleSetup/Shortcut-Handles
+mv @infect /$HOME/SingleSetup/Shortcut-Handles
+mv @pswd /$HOME/SingleSetup/Shortcut-Handles
+mv loveme /$HOME/SingleSetup/Shortcut-Handles
+mv @tload /$HOME/SingleSetup/Shortcut-Handles
+mv motd /$HOME/SingleSetup/Shortcut-Handles
+mv bash.bashrc /$HOME/SingleSetup/Shortcut-Handles
+mv c /$HOME/SingleSetup/Shortcut-Handles
+
+
+echo "STATUS 100 PERCENT COMPLETED "
+clear
+
+
+
+
+loveme
